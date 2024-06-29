998a1f5496c545fb37991e41c8fe783987799929
ding99ya <102636654+ding99ya@users.noreply.github.com>
2024-06-29 14:50:19 -0500

    Update Vercel Config

--- server/server.js
+++ server/server.js
@@ -13,9 +13,6 @@ const PresaleUserModel = require("./models/PresaleUserModel.js");
 const path = require("path");
 const axios = require("axios");
 const { PrivyClient } = require("@privy-io/server-auth");
-// const {
-//   default: InviteCode,
-// } = require("../client/src/components/InviteCode.jsx");
 
 require("dotenv").config();
 
