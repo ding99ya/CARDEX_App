690d21c3861ae00c22e9f7421639f12c4194430a
ding99ya <102636654+ding99ya@users.noreply.github.com>
2024-07-25 23:07:33 -0500

    Update Market UI

--- client/src/components/Market.jsx
+++ client/src/components/Market.jsx
@@ -12,28 +12,30 @@ function Market() {
   };
 
   return (
-    <div className="bg-white flex items-center justify-center min-h-screen">
-      <div class="container mx-auto px-8 lg:px-0">
+    <div className="bg-white flex items-center justify-center">
+      <div class="container mx-auto px-4 lg:px-0">
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <div
             className="w-full mt-4 cursor-pointer bg-white rounded-3xl overflow-hidden hover:bg-gray-100 transition duration-300 ease-in-out border-2 border-black"
             onClick={() => handleShopClick("/Market/Presale")}
           >
-            <img
-              src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailone.jpg"
-              alt="Presale"
-              className="w-full h-56 object-cover"
-            />
+            <div className="relative w-full pb-[50.0%] lg:pb-[50.0%]">
+              <img
+                src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailone.jpg"
+                alt="Presale"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+              />
+            </div>
 
             <div className="p-4 flex justify-between items-end items-center font-helvetica-neue">
               <div>
-                <p className="text-lg font-bold">Presale</p>
+                <p className="text-lg font-bold">PRESALE</p>
               </div>
               <button
                 className="bg-white text-black font-bold font-helvetica-neue border border-black px-4 py-2 rounded-full hover:bg-black hover:text-white transition duration-300 ease-in-out"
                 onClick={() => handleShopClick("/Market/Presale")}
               >
-                Trade
+                COLLECT
               </button>
             </div>
           </div>
@@ -41,21 +43,23 @@ function Market() {
             className="w-full mt-4 cursor-pointer bg-white rounded-3xl overflow-hidden hover:bg-gray-100 transition duration-300 ease-in-out border-2 border-black"
             onClick={() => handleShopClick("/Market/Pokemon")}
           >
-            <img
-              src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailone.jpg"
-              alt="Pokemon"
-              className="w-full h-56 object-cover"
-            />
+            <div className="relative w-full pb-[50.0%] lg:pb-[50.0%]">
+              <img
+                src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailone.jpg"
+                alt="Presale"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+              />
+            </div>
 
             <div className="p-4 flex justify-between items-end items-center font-helvetica-neue">
               <div>
-                <p className="text-lg font-bold">Pokemon</p>
+                <p className="text-lg font-bold">POKEMON</p>
               </div>
               <button
                 className="bg-white text-black font-bold font-helvetica-neue border border-black px-4 py-2 rounded-full hover:bg-black hover:text-white transition duration-300 ease-in-out"
                 onClick={() => handleShopClick("/Market/Pokemon")}
               >
-                Trade
+                COLLECT
               </button>
             </div>
           </div>
@@ -63,21 +67,23 @@ function Market() {
             className="w-full mt-4 cursor-pointer bg-white rounded-3xl overflow-hidden hover:bg-gray-100 transition duration-300 ease-in-out border-2 border-black"
             onClick={() => handleShopClick("/Market/Basketball")}
           >
-            <img
-              src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailtwo.jpg"
-              alt="Basketball"
-              className="w-full h-56 object-cover"
-            />
+            <div className="relative w-full pb-[50.0%] lg:pb-[50.0%]">
+              <img
+                src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailtwo.jpg"
+                alt="Presale"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+              />
+            </div>
 
             <div className="p-4 flex justify-between items-end items-center font-helvetica-neue">
               <div>
-                <p className="text-lg font-bold">Basketball</p>
+                <p className="text-lg font-bold">BASKETBALL</p>
               </div>
               <button
                 className="bg-white text-black font-bold font-helvetica-neue border border-black px-4 py-2 rounded-full hover:bg-black hover:text-white transition duration-300 ease-in-out"
                 onClick={() => handleShopClick("/Market/Basketball")}
               >
-                Trade
+                COLLECT
               </button>
             </div>
           </div>
@@ -85,21 +91,23 @@ function Market() {
             className="w-full mt-4 cursor-pointer bg-white rounded-3xl overflow-hidden hover:bg-gray-100 transition duration-300 ease-in-out border-2 border-black"
             onClick={() => handleShopClick("/Market/Baseball")}
           >
-            <img
-              src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailthree.jpg"
-              alt="Baseball"
-              className="w-full h-56 object-cover"
-            />
+            <div className="relative w-full pb-[50.0%] lg:pb-[50.0%]">
+              <img
+                src="https://cardsimage.s3.amazonaws.com/CardClassificationImage/detailthree.jpg"
+                alt="Presale"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+              />
+            </div>
 
             <div className="p-4 flex justify-between items-end items-center font-helvetica-neue">
               <div>
-                <p className="text-lg font-bold">Baseball</p>
+                <p className="text-lg font-bold">BASEBALL</p>
               </div>
               <button
                 className="bg-white text-black font-bold font-helvetica-neue border border-black px-4 py-2 rounded-full hover:bg-black hover:text-white transition duration-300 ease-in-out"
                 onClick={() => handleShopClick("/Market/Baseball")}
               >
-                Trade
+                COLLECT
               </button>
             </div>
           </div>
