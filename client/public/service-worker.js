da7555dd229eb4f262aada9bb190be57b477e69c
ding99ya <102636654+ding99ya@users.noreply.github.com>
2024-08-18 15:52:06 -0500

    Update

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,6 +1,24 @@
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
+);
+
 const CACHE_NAME = "Cardex-cache-v1";
-// Cached URL
 const urlsToCache = ["/", "/index.html", "/manifest.json", "/Cardex_logo.png"];
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzt0L-e0BHdgRTIUHYaxzOdhDxdb2FnSs",
+  authDomain: "cardex-96b70.firebaseapp.com",
+  projectId: "cardex-96b70",
+  storageBucket: "cardex-96b70.appspot.com",
+  messagingSenderId: "447559129333",
+  appId: "1:447559129333:web:471fabe7db5855d57e8dbc",
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -32,4 +50,18 @@ self.addEventListener("activate", (event) => {
       );
     })
   );
+});
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/Cardex_logo.png",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,6 +6,9 @@ import { AuthProvider } from "./components/AuthContext";
 import { base, baseSepolia, mainnet } from "viem/chains";
 import { Buffer } from "buffer";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from "firebase/app";
+
 window.Buffer = Buffer;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -37,3 +40,44 @@ root.render(
 );
 
 serviceWorkerRegistration.register();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzt0L-e0BHdgRTIUHYaxzOdhDxdb2FnSs",
+  authDomain: "cardex-96b70.firebaseapp.com",
+  projectId: "cardex-96b70",
+  storageBucket: "cardex-96b70.appspot.com",
+  messagingSenderId: "447559129333",
+  appId: "1:447559129333:web:471fabe7db5855d57e8dbc",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(firebaseApp);
+
+const requestNotificationPermission = async () => {
+  try {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      const token = await getToken(messaging, {
+        vapidKey:
+          "BKnjduM6elMnouX5rA_Qh0RsF5W3o6doc_4WaBV_PaRErnNdg6v_0bgphpdqhS9Y0_ZJg7HftVbWbuIf7lQPo4Q",
+      });
+      console.log("Notification permission granted. Token:", token);
+      // You can subscribe to a topic here if needed
+      // messaging.subscribeToTopic(token, 'all_users');
+    } else {
+      console.log("Notification permission denied");
+    }
+  } catch (error) {
+    console.error("Error requesting notification permission:", error);
+  }
+};
+
+// Handle foreground messages
+onMessage(messaging, (payload) => {
+  console.log("Message received in the foreground:", payload);
+  const { title, body } = payload.notification;
+  new Notification(title, { body });
+});
+
+// Request permission when the app loads
+requestNotificationPermission();
