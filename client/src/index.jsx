fbb8716db202df3b490500f595ffe6e173b46176
ding99ya <102636654+ding99ya@users.noreply.github.com>
2024-08-24 21:39:36 -0500

    Update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -113,7 +113,7 @@ root.render(
       appearance: {
         theme: "light",
         accentColor: "#676FFF",
-        logo: "/Cardex_logo.png",
+        logo: "/Cardex.jpg",
       },
       // Create embedded wallets for users who don't have a wallet
       embeddedWallets: {
